bfa496cb4b2d5414a819333ba490ea6f2313adda
singh8x1 <singh8x1@uwindsor.ca>
2022-11-21 00:20:56 -0500

    INTILAL Commit for back-end

---
+++ backend/models/user.model.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const User = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+const model = mongoose.model("UserData", User);
+
+export default model;
---
+++ backend/routes/routes.js
@@ -0,0 +1,15 @@
+import express from "express";
+import {
+  getPosts,
+  createPosts,
+  updatePost,
+  deletePost,
+} from "../controllers/posts.js";
+const router = express.Router();
+
+router.get("/", getPosts);
+router.post("/", createPosts);
+router.patch("/:id", updatePost);
+router.delete("/:id", deletePost);
+
+export default router;
